040f3f7b16f8233c4464cb6b538e30009bbcb58e
3rikMtz <eri0305@hotmail.com>
2023-07-22 13:20:09 -0600

    all

--- models/index.js
+++ models/index.js
@@ -1,39 +1,43 @@
-// db/models/index.js
+'use strict';
 
-const User = require('./user');
-const Blog = require('./blog');
-const Comment = require('./comment');
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const process = require('process');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
 
-User.hasMany(Blog, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE',
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (
+      file.indexOf('.') !== 0 &&
+      file !== basename &&
+      file.slice(-3) === '.js' &&
+      file.indexOf('.test.js') === -1
+    );
+  })
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
 });
 
-Blog.belongsTo(User, {
-  foreignKey: 'user_id',
-});
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
-Blog.hasMany(Comment, {
-  foreignKey: 'blog_id',
-  onDelete: 'CASCADE',
-});
-
-Comment.belongsTo(Blog, {
-  foreignKey: 'blog_id',
-});
-
-User.hasMany(Comment, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE',
-});
-
-Comment.belongsTo(User, {
-  foreignKey: 'user_id',
-});
-
-
-module.exports = {
-  User,
-  Blog,
-  Comment,
-};
+module.exports = db;
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const sequelize = require('./config/connection');
+const sequelize = require('./config/config');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
