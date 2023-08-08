abf523998fbd7babdbb7568f0d087bed9b28339c
Jonathan Cobos <jonathan.cobos@outlook.es>
2023-08-07 22:17:34 -0600

    initial commit

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,27 +1,17 @@
+const sequelize = require("../config/connection");
+const { User, Post, Comment } = require("../models");
+
+const userData = require("./userData.json");
+const postData = require("./postData.json");
+const commentData = require("./commentData.json");
+
 const seedDatabase = async () => {
-  try {
-    // Ensure database connection and table schema are created
-    await sequelize.sync();
+  await sequelize.sync({ force: true });
+  await User.bulkCreate(userData);
+  await Post.bulkCreate(postData);
+  await Comment.bulkCreate(commentData);
 
-    // Drop tables in the correct order: Comments, Posts, User
-    await Comment.destroy({ truncate: true });
-    await Post.destroy({ truncate: true });
-    await User.destroy({ truncate: true });
-
-    // Seed users
-    await User.bulkCreate(userData);
-
-    // Seed posts
-    await Post.bulkCreate(postData);
-
-    // Seed comments
-    await Comment.bulkCreate(commentData);
-
-    console.log('Database seeded successfully!');
-    process.exit(0);
-  } catch (err) {
-    console.error('Error seeding database:', err);
-    process.exit(1);
-  }
+  process.exit(0);
 };
 
+seedDatabase();
