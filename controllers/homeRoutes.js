2b226376389c3c09fd1d1fad9f60033b9773ac7e
Jonathan Cobos <jonathan.cobos@outlook.es>
2023-08-07 22:24:47 -0600

    another commit

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,24 +4,9 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    // Get all projects and JOIN with user data
-    const postData = await Post.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['username'],
-        },
-      ],
-    });
+    res.render('homepage')
 
-    // Serialize data so the template can read it
-    const projects = postData.map((post) => post.get({ plain: true }));
 
-    // Pass serialized data and session flag into template
-    res.render('homepage', { 
-      posts, 
-      logged_in: req.session.logged_in 
-    });
   } catch (err) {
     res.status(500).json(err);
   }
